1b0cba303273affa690ff309e4de8858289fe1e4
gabrielChaves7 <gabrielbum@gmail.com>
2019-09-23 21:47:15 -0300

    logica para calcular quantas operações são necessárias finalizada

--- main/main/tratar_entrada.h
+++ main/main/tratar_entrada.h
@@ -8,17 +8,59 @@
 // TODO: Reference additional headers your program requires here.
 
 std::list<int> listaRecipientes;
+std::list<int> listaResultados;
+std::list<int> listaAuxiliar;
+
+bool verificaElementoJaExisteNaLista(int elemento) {
+	std::list<int>::iterator it;
+	it = std::find(listaResultados.begin(), listaResultados.end(), elemento);
+	if (it != listaResultados.end())
+		return true;
+
+	return false;
+}
+
+void adicionaNovasCombinacoesDeQuantidades() {
+	for (int recipiente : listaRecipientes) {
+		for (int recipientesFormado : listaAuxiliar) {
+			int soma = recipiente + recipientesFormado;
+			int subtracao = recipiente - recipientesFormado;
+			if (subtracao > 0 && !verificaElementoJaExisteNaLista(subtracao)) {
+				listaResultados.push_back(subtracao);
+			}
+
+			if (soma > 0 && !verificaElementoJaExisteNaLista(soma)) {
+				listaResultados.push_back(soma);
+			}
+		}
+	}
+}
 
 void tratarEntrada(int quantidade, char evento) {
+	bool parar = false;
+	int numeroOperacoes = 1;
 	switch (evento)
 	{
 	case 'i':
 		printf("\n Inclusão\n");
 		listaRecipientes.push_back(quantidade);
 		break;
-	case 'p':
-		for (int val : listaRecipientes)
-			std::cout << val << ",";
+	case 'p':		
+		listaResultados.clear();
+		listaAuxiliar.clear();
+		listaAuxiliar.push_back(0);
+		while (parar == false) {						
+			adicionaNovasCombinacoesDeQuantidades();
+			if (verificaElementoJaExisteNaLista(quantidade)) {
+				printf("O numero de operações necessárias foi: %d", numeroOperacoes);
+				parar = true;
+			}
+
+			listaAuxiliar = listaResultados;
+
+			numeroOperacoes++;
+		}
+		
 		printf("\n Medir\n");
 		break;
 	case 'r':
